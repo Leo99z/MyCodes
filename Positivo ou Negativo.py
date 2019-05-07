ffadb806b43173aff8432c737b7238a690f4c854
Leonardo Ribeiro Leonardi <49618344+Leo99z@users.noreply.github.com>
2019-05-07 14:17:50 -0300

    Add files via upload
    This script checks whether a number is positive or negative
    Este script verifica se um número é positivo ou negativo

---
+++ Positivo ou Negativo.py
@@ -0,0 +1,13 @@
+num=int(input("Digite um número: "))  #Usuário informa o número 
+
+if num > 0:   #Verifica se o número é maior que 0 
+    print("O número digitado é Positivo.")  #Se maior que 0, ele é positivo
+else:
+    if num == 0: #Verifica se ele é igual a 0
+        print("O número digitado não é nem positivo e nem negativo, ele é 0.") #Se igual a 0, mostrar que ele não é nem positivo e nem negativo, mas sim 0
+    else:  #Se o número não for nem maior e nem igual a 0. Ele é negativo
+        print("O número digitado é Negativo.")
+
+#Leonardo Ribeiro Leonardi
+#Descobrir se o número digitado é positivo ou negativo.
+#07/05/2019
